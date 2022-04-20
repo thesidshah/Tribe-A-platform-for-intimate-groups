32a630acd80062e3bc91c0b0a14450cc6c1c9682
Rishita <reddy.ri@northeastern.edu>
2022-04-20 01:26:47 -0400

    edit expense changes

--- controllers/expense-controller.js
+++ controllers/expense-controller.js
@@ -7,6 +7,7 @@ export default (app) => {
     app.get('/famjam/group1/expense', findAllExpenses);
     app.post('/famjam/group1/expense', createExpense);
     app.delete('/famjam/group1/expense/:exId', deleteExpense);
+    app.put('/famjam/group1/expense/:exId', updateExpense);
 }
 
 const createExpense = async  (req, res) => {
@@ -23,5 +24,12 @@ const findAllExpenses = async (req, res) => {
 const deleteExpense = async (req, res) => {
     const expenseIdToDelete = req.params.exId;
     const status = await expenseDao.deleteExpense(expenseIdToDelete);
+    res.send(status);
+}
+
+const updateExpense = async (req, res) => {
+    const expenseIdToUpdate = req.params.exId;
+    const updatedExpense = req.body;
+    const status = await expenseDao.updateExpense(expenseIdToUpdate, updatedExpense);
     res.send(status);
 }
--- databases/dao/expense-dao.js
+++ databases/dao/expense-dao.js
@@ -1,7 +1,6 @@
 import expenseModel from "../../model/expense-model.js";
 
-
 export const findAllExpenses = () => expenseModel.find();
 export const createExpense = (expense) => expenseModel.create(expense);
 export const deleteExpense = (exId) => expenseModel.deleteOne({_id: exId});
-// export const updateExpense = (exId, expense) => expenseModel.updateOne({_id: exId}, {$set: expense})
+export const updateExpense = (exId, expense) => expenseModel.updateOne({_id: exId}, {$set: expense})
