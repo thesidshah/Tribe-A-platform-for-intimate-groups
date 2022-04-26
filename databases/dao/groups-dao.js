69762a518c5a1384da1f53e5f5e8de9513277649
Avanti Dorle <avantidorle@Avantis-MacBook-Air.local>
2022-04-26 02:32:42 -0400

    Add and delete group, groups settings page

--- databases/dao/groups-dao.js
+++ databases/dao/groups-dao.js
@@ -2,6 +2,8 @@ import groupsModel from "../../model/groups-model.js";
 
 export const findAllGroups = () => groupsModel.find();
 export const findByGroupId = (gid) => groupsModel.findById(gid);
+export const createGroup = (group) => groupsModel.create(group);
+export const deleteGroup = (gid) => groupsModel.deleteOne({_id: gid});
 export const addSectionInGroup = (sid, gid) => groupsModel.updateOne({_id: gid},{"$push": {"todoSectionIds": sid} });
 export const addExpenseInGroup = (exId, gid) => groupsModel.updateOne({_id: gid},{"$push": {"expensesIds": exId} });
 export const deleteSectionFromGroup = (sid, gid) => groupsModel.updateOne({_id: gid}, {"$pull": {"todoSectionIds": sid}});
