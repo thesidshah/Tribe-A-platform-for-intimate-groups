124e29679ba0414ba9ba201adcc483229e571855
Siddhant Shah <siddhant997@gmail.com>
2022-05-01 20:14:06 -0400

    Integrating calendar id

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -7,6 +7,7 @@ export default (app) => {
     app.post('/famjam/users', createUser);
     app.get('/famjam/users/:email', findOneUser);
     app.put('/famjam/users/:usersId', updateUsers);
+    app.get('/famjam/users/id/:usersId', findUserId);
 }
 
 const findAllUsers = async (req, res) => {
@@ -26,9 +27,15 @@ const createUser = async  (req, res) => {
 }
 
 const updateUsers = async (req, res) => {
-console.log("rfbee");
     const usersIdToUpdate = req.params.usersId;
     const updatedUsers = req.body;
     const status = await usersDao.updateUsers(usersIdToUpdate, updatedUsers);
     res.send(status);
 }
+
+const findUserId = async (req, res) => {
+    const users = await usersDao.findUserById(req.params.usersId);
+    console.log(users);
+    res.json(users);
+}
+
--- databases/dao/users-dao.js
+++ databases/dao/users-dao.js
@@ -6,3 +6,4 @@ export const findAllUsers = () => usersModel.find();
 export const createUser = (user) => usersModel.create(user);
 export const updateUsers = (usersId, users) =>
    usersModel.updateOne({_id: usersId}, {$set: users})
+export const findUserById = (usersId) => usersModel.findById(usersId);
